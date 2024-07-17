8b90f7c63e14e57091c5d4765a5ce8cb960bc1d4
ejjagirikishore <ejjagirikishor@gmail.com>
2024-07-17 12:15:27 +0530

    commit

---
+++ CMS-login/src/main/java/com/ty/demo/CmsLoginApplication.java
@@ -0,0 +1,13 @@
+package com.ty.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmsLoginApplication.class, args);
+	}
+
+}
